f9f14a0ea828f64b009e3ec5e3764bfb7311d477
Yan, Bo <bo.yan@microfun.com>
2024-03-05 10:58:24 +0800

    set Dump syncmate log level to INFO. (Logging is too frequent when LOG Level)

--- src/backend/utils/time/sharedsnapshot.c
+++ src/backend/utils/time/sharedsnapshot.c
@@ -653,7 +653,7 @@ dumpSharedLocalSnapshot_forCursor(void)
 	pDump->distributedXid = src->distributedXid;
 	pDump->localXid = src->fullXid;
 
-	elog(LOG, "Dump syncmate : %u snapshot to slot %d", src->segmateSync, id);
+	elog(INFO, "Dump syncmate : %u snapshot to slot %d", src->segmateSync, id);
 
 	src->cur_dump_id =
 		(src->cur_dump_id + 1) % SNAPSHOTDUMPARRAYSZ;
